2dab31247a1b700b55001786482eb0d82203b9e3
Deja  Drummer <dejadrummer@Dejas-MacBook-Air.local>
2022-05-28 20:48:10 -0400

    05/28

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//Leetcode Breakdown 
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+
+var missingNumber = function(nums) {
+  let sum = 0, total = 0
+    for(let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        total += i + 1
+    }
+    return total - sum     
+  
+    } 
+
